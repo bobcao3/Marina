b479db132420aa39566d38606ae046972fe5816d
Bob Cao <bobcaocheng@gmail.com>
2019-01-03 21:05:40 +0800

    seat focus & cursor handling
    Signed-off-by: Bob Cao <bobcaocheng@gmail.com>

--- src/marina-seat.hpp
+++ src/marina-seat.hpp
@@ -26,10 +26,18 @@ public:
 
     struct wlr_seat* seat;
 
+    struct wlr_surface* active_surface = NULL;
+    MarinaView* active_view = NULL;
+
     void add_keyboard(MarinaInput* input);
     void add_pointer(MarinaInput* input);
     void add_touch(MarinaInput* input);
     void update_capabilities();
+
+    void focus_view(MarinaView* view, struct wlr_surface* surface);
+
+    void process_cursor_motion(struct wlr_cursor* wlr_cursor, uint32_t time);
+    void process_cursor_button(struct wlr_cursor* wlr_cursor, uint32_t time, uint32_t button, uint32_t state);
 
     MarinaSeat(MarinaServer* server, const char* name);
     ~MarinaSeat();
--- src/seat.cpp
+++ src/seat.cpp
@@ -50,3 +50,50 @@ void MarinaSeat::update_capabilities() {
 
     // TODO： Hide cursor when there isn't one
 }
+
+void MarinaSeat::process_cursor_motion(struct wlr_cursor* wlr_cursor, uint32_t time) {
+    double sx, sy;
+    struct wlr_surface* surface = NULL;
+    MarinaView* view = MarinaView::view_at(this->server, wlr_cursor->x, wlr_cursor->y, &surface, &sx, &sy);
+
+    if (surface) {
+        wlr_seat_pointer_notify_enter(this->seat, surface, sx, sy);
+        if (time > 0) {
+            wlr_seat_pointer_notify_motion(this->seat, time, sx, sy);        
+        }
+    } else {
+        wlr_seat_pointer_clear_focus(this->seat);
+    }
+}
+
+void MarinaSeat::process_cursor_button(struct wlr_cursor* wlr_cursor, uint32_t time, uint32_t button, uint32_t state) {
+    double sx, sy;
+    struct wlr_surface* surface = NULL;
+    MarinaView* view = MarinaView::view_at(this->server, wlr_cursor->x, wlr_cursor->y, &surface, &sx, &sy);
+
+    if (view != this->active_view || surface != this->active_surface) focus_view(view, surface);
+
+    if (time > 0) wlr_seat_pointer_send_button(this->seat, time, button, state);
+}
+
+void MarinaSeat::focus_view(MarinaView* view, struct wlr_surface* surface) {
+    wlr_log(WLR_DEBUG, "Focus view %x", view);
+
+    if (this->active_view) {
+        this->active_view->deactivate();
+    }
+
+    this->active_view = view;
+    this->active_surface = surface;
+
+    if (view && surface) {
+        view->activate();
+
+        struct wlr_keyboard *keyboard = wlr_seat_get_keyboard(this->seat);
+        if (keyboard) {
+            wlr_seat_keyboard_notify_enter(this->seat, surface, keyboard->keycodes, keyboard->num_keycodes, &keyboard->modifiers);
+        } else {
+            wlr_seat_keyboard_notify_enter(this->seat, surface, NULL, 0, NULL);
+        }
+    }
+}
